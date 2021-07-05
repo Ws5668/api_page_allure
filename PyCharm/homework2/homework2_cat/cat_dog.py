6e6d7c2c57c6bc357f9f58e57adb4bfefb8e260b
王森 <1263991649@qq.com>
2021-07-05 20:14:19 +0800

    猫狗实战作业

---
+++ PyCharm/homework2/homework2_cat/cat_dog.py
@@ -0,0 +1,59 @@
+class Animal:
+
+
+    def __init__(self ,name ,color ,age ,gender):
+        self.animal_name = name
+        self.animal_color = color
+        self.animal_age = age
+        self.animal_gender = gender
+
+    def jiao(self):
+        print(f"这只{self.animal_name},是{self.animal_color}颜色的,今年{self.animal_age}岁,性别是:{self.animal_gender} 它会叫~")
+
+    def run(self):
+        print(f"这只{self.animal_name},是{self.animal_color}颜色的,今年{self.animal_age}岁,性别是:{self.animal_gender} 它会跑~")
+
+
+class Cat(Animal):
+    def __init__(self,name,color,age,gender,hair):
+
+
+        super().__init__(name,color,age,gender)
+        self.hair = "短毛"
+
+
+    def zhuo_mouse(self):
+        print(f"这只{self.animal_name}是{self.animal_color}色{self.hair}的，今年{self.animal_age}岁，性别是:{self.animal_gender},并且会抓老鼠。")
+
+    def jiao(self):
+        print(f"这只{self.animal_name}是{self.animal_color}色{self.hair}的，今年{self.animal_age}岁，性别是:{self.animal_gender},发出喵喵的叫声。")
+
+class Dog(Animal):
+    def __init__(self,name,color,age,gender,hair):
+
+        super().__init__(name,color,age,gender)
+        self.hair = "长毛"
+
+
+    def kan_jia(self):
+        print(f"这只{self.animal_name}是{self.animal_color}色{self.hair}的，今年{self.animal_age}岁，性别是:{self.animal_gender},并且会看家。")
+
+    def jiao(self):
+        print(f"这只{self.animal_name}是{self.animal_color}色{self.hair}的，今年{self.animal_age}岁，性别是:{self.animal_gender},并且会汪汪叫。")
+
+if __name__ == "main":
+    print("猫类验证")
+    mao = Cat("猫","黑",3,"雄性","短毛")
+    mao.zhuo_mouse()
+    mao.jiao()
+    print("狗类验证")
+    gou = Dog("狗","黄",2,"雌性","长毛")
+    gou.kan_jia()
+    gou.jiao()
+
+
+
+
+
+
+
